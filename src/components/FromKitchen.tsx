e5004cc14cc27a25698150f8b0cbbff3959ddc14
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-01-19 21:52:32 +0100

    Kitchen

--- src/components/FromKitchen.tsx
+++ src/components/FromKitchen.tsx
@@ -1,8 +1,10 @@
-
+import Burger from "../assets/images/Burger.png";
 export const Kitchen = () => {
     return (
         <div className="pt-5  md:py-20  md:px-28 w-full h-auto bg-[#EBE2E4] relative">
             <div className="flex justify-between">
+                <img src={Burger} alt="" />
+               
                 </div>
         </div>
     )
