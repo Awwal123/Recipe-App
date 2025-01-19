542890772b6bdb0e6b1ee2e34554f6c95295fe2c
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-01-19 22:44:05 +0100

    adjustment

--- src/components/FromKitchen.tsx
+++ src/components/FromKitchen.tsx
@@ -7,32 +7,32 @@ import Cake from "../assets/images/Cake.png";
 
 export const Kitchen = () => {
   return (
-    <div className="pt-5 md:py-20 md:px-28 w-full h-auto bg-[#EBE2E4] relative">
+    <div className="py-20 md:py-20 md:px-28 w-full h-auto bg-[#EBE2E4] relative">
       {/* Background Decorative Icons */}
-      <img src={Peer} alt="Peer" className="absolute bottom-[10%] right-[20%] w-[120px]" />
-      <img src={LeftPizza} alt="Left Pizza" className="absolute top-[40%] right-[0%] w-[208px]" />
-      <img src={RightPizza} alt="Right Pizza" className="absolute top-[60%] left-[40%] w-[208px]" />
-      <img src={Cake} alt="Cake" className="absolute top-[10%] left-[45%] w-[183px]" />
+      <img src={Peer} alt="Peer" className="absolute bottom-[10%] right-[20%] w-14 md:w-[120px]" />
+      <img src={LeftPizza} alt="Left Pizza" className="absolute top-[40%] hidden md:block right-[0%] w-[208px]" />
+      <img src={RightPizza} alt="Right Pizza" className="absolute top-[50%] md:top-[60%] md:left-[40%] w-20 md:w-[208px]" />
+      <img src={Cake} alt="Cake" className="absolute top-[5%] w-16 md:top-[10%] left-2 md:left-[45%] md:w-[183px]" />
 
       {/* Main Content */}
-      <div className="flex flex-col md:flex-row justify-center gap-5 md:gap-52 relative items-center">
+      <div className="flex flex-col md:flex-row w-full md:justify-center gap-5 md:gap-52 relative items-center">
         {/* Image Section */}
-        <img src={Burger} alt="Burger" className="w-[300px] md:w-[400px] md:rounded-tl-[50px] md:rounded-br-[50px] shadow-lg" />
+        <img src={Burger} alt="Burger" className="w-[80vw] h-[400px]  md:h-auto md:w-[400px] rounded-tl-[50px] rounded-br-[50px] shadow-lg" />
 
         {/* Text Section */}
-        <div className="flex flex-col gap-4 items-start">
-          <h1 className="md:text-5xl text-4xl font-bold bg-gradient-to-r from-[#C91937] to-[#FDB646] text-transparent bg-clip-text leading-tight">
+        <div className="flex flex-col mt-9 gap-4 items-center md:items-start">
+          <h1 className="md:text-5xl text-4xl font-bold bg-gradient-to-r from-[#C91937] to-[#FDB646] text-transparent text-center bg-clip-text leading-tight">
             From Kitchen to Table
           </h1>
-          <h1 className="md:text-5xl text-4xl font-bold bg-gradient-to-r from-[#C91937] to-[#FDB646] text-transparent bg-clip-text leading-tight">
+          <h1 className="md:text-5xl text-4xl font-bold bg-gradient-to-r from-[#C91937] to-[#FDB646] text-transparent text-center bg-clip-text leading-tight">
             Discover Your Next
           </h1>
-          <h1 className="md:text-5xl text-4xl font-bold bg-gradient-to-r from-[#C91937] to-[#FDB646] text-transparent bg-clip-text leading-tight">
+          <h1 className="md:text-5xl text-4xl font-bold bg-gradient-to-r from-[#C91937] to-[#FDB646] text-transparent text-center bg-clip-text leading-tight">
             Delicious Dish!
           </h1>
 
           {/* Buttons */}
-          <div className="flex gap-4 mt-5">
+          <div className="flex gap-4 my-5 pb-8">
             <button className="bg-[#C91937] text-white py-2 px-6 rounded-tl-2xl rounded-br-2xl hover:bg-[#A3142B] font-semibold">
               Register
             </button>
