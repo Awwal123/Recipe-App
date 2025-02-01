1042315044c9df9d892b5ca076c2d3be7e7d2967
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-02-01 20:42:34 +0100

    20% completion

--- src/components/FromKitchen.tsx
+++ src/components/FromKitchen.tsx
@@ -9,16 +9,33 @@ import { Link } from "react-router-dom";
 export const Kitchen = () => {
   return (
     <div className="py-20 md:py-20 md:px-28 w-full h-auto bg-[#EBE2E4] relative">
-      <img src={Peer} alt="Peer" className="absolute top-3 md:bottom-[10%] right-5 m:right-[20%] w-14 md:w-[120px]" />
-      <img src={LeftPizza} alt="Left Pizza" className="absolute md:top-[40%] bottom-[10%]  right-7 md:right-[0%]  w-20 md:w-[208px]" />
-      <img src={RightPizza} alt="Right Pizza" className="absolute top-[50%] md:top-[60%] md:left-[40%] w-20 md:w-[208px]" />
-      <img src={Cake} alt="Cake" className="absolute top-[5%] w-16 md:top-[10%] left-2 md:left-[45%] md:w-[183px]" />
+      <img
+        src={Peer}
+        alt="Peer"
+        className="absolute top-3 md:bottom-[10%] right-5 m:right-[20%] w-14 md:w-[120px]"
+      />
+      <img
+        src={LeftPizza}
+        alt="Left Pizza"
+        className="absolute md:top-[40%] bottom-[10%]  right-7 md:right-[0%]  w-20 md:w-[208px]"
+      />
+      <img
+        src={RightPizza}
+        alt="Right Pizza"
+        className="absolute top-[50%] md:top-[60%] md:left-[40%] w-20 md:w-[208px]"
+      />
+      <img
+        src={Cake}
+        alt="Cake"
+        className="absolute top-[5%] w-16 md:top-[10%] left-2 md:left-[45%] md:w-[183px]"
+      />
 
       <div className="flex flex-col md:flex-row w-full md:justify-center gap-5 md:gap-52 relative items-center">
-       
-        <img src={Burger} alt="Burger" className="w-[80vw] h-[400px]  md:h-auto md:w-[400px] rounded-tl-[50px] rounded-br-[50px] shadow-lg" />
-
-        {/* Text Section */}
+        <img
+          src={Burger}
+          alt="Burger"
+          className="w-[80vw] h-[400px]  md:h-auto md:w-[400px] rounded-tl-[50px] rounded-br-[50px] shadow-lg"
+        />
         <div className="flex flex-col mt-9 gap-4 items-center md:items-start">
           <h1 className="md:text-5xl text-4xl font-bold bg-gradient-to-r from-[#C91937] to-[#FDB646] text-transparent text-center bg-clip-text leading-tight">
             From Kitchen to Table
@@ -29,15 +46,18 @@ export const Kitchen = () => {
           <h1 className="md:text-5xl text-4xl font-bold bg-gradient-to-r from-[#C91937] to-[#FDB646] text-transparent text-center bg-clip-text leading-tight">
             Delicious Dish!
           </h1>
-
-          {/* Buttons */}
           <div className="flex gap-4 my-5 pb-8">
-        <Link to={"/signup"}>    <button className="bg-[#C91937] text-white py-2 px-6 rounded-tl-2xl rounded-br-2xl hover:bg-[#A3142B] font-semibold">
-              Register
-            </button> </Link>
-      <Link to={"/recipes"}><button className="bg-[#FDB646] text-white py-2 px-6 rounded-tl-2xl rounded-br-2xl hover:bg-[#E69C32] font-semibold">
-              Explore
-            </button></Link>
+            <Link to={"/signup"}>
+              {" "}
+              <button className="bg-[#C91937] text-white py-2 px-6 rounded-tl-2xl rounded-br-2xl hover:bg-[#A3142B] font-semibold">
+                Register
+              </button>{" "}
+            </Link>
+            <Link to={"/recipes"}>
+              <button className="bg-[#FDB646] text-white py-2 px-6 rounded-tl-2xl rounded-br-2xl hover:bg-[#E69C32] font-semibold">
+                Explore
+              </button>
+            </Link>
           </div>
         </div>
       </div>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,73 +10,68 @@ import { Link } from "react-router-dom";
 export const HeroSection = () => {
   return (
     <div className="md:pb-14 z-10">
-    <div className="pt-5  md:py-7  md:px-28 w-full h-auto   relative">
-      <img
-        src={Cup}
-        alt="Cup"
-        className="absolute top-96 right-11 w-[110px] h-auto"
-      />
-      <img
-        src={Niddle}
-        alt="Niddle"
-        className="absolute bottom-60 right-96 transform -translate-x-1/2 w-auto h-auto"
-      />
+      <div className="pt-5  md:py-7  md:px-28 w-full h-auto   relative">
+        <img
+          src={Cup}
+          alt="Cup"
+          className="absolute top-96 right-11 w-[110px] h-auto"
+        />
+        <img
+          src={Niddle}
+          alt="Niddle"
+          className="absolute bottom-60 right-96 transform -translate-x-1/2 w-auto h-auto"
+        />
 
-      <img
-        src={PineApple}
-        alt="Cake"
-        className="absolute right-1/3 top-40 transform -translate-y-1/2 w-auto h-auto"
-      />
-      <div className="flex justify-between md:flex-row flex-col md:py-7 relative">
-        <div className="flex flex-col items-center gap-10 md:gap-20 justify-center">
-          <div className="flex flex-col">
-            <h1 className="md:text-6xl text-4xl  font-bold bg-gradient-to-r  from-[#C91937] to-[#FDB646] text-transparent bg-clip-text  w-full text-center md:h-[122px] h-auto ">
-              Far from home?
-            </h1>
-            <p className="text-2xl px-5  text-center md:-mt-9">
-              we got you, checkout recipes from home chef's
-            </p>
-          </div>
-
-          <div className="flex md:flex-row flex-col justify-between gap-10 md:gap-16">
-            <Link to="/signup">
-              <div className="flex items-center cursor-pointer justify-center rounded-tl-3xl shadow-custom rounded-br-3xl  bg-[#C91937] text-white w-[149px] h-[47px]">
-                Register
-              </div>
-            </Link>
-            <Link to={"/recipes"}>
-            <div className="flex items-center cursor-pointer justify-center rounded-tl-3xl shadow-custom rounded-br-3xl  bg-[#FDB646] text-white w-[149px] h-[47px]">
-              Explore
-            </div>
-            </Link>
-          </div>
-
-          <div className="flex  my-5 md:mt-20 items-center justify-center w-[100%] gap-10">
-            <img src={Twitter} alt="" />
-            <img src={Github} alt="" />
-
-            <div className="w-[45px] h-[45px] rounded-full p-[2px] bg-gradient-to-r from-red-500 to-yellow-500">
-              <div className="w-full h-full flex items-center justify-center rounded-full bg-white">
-                <img
-                  src={din}
-                  alt=""
-                  className=""
-                />
-              </div>
+        <img
+          src={PineApple}
+          alt="Cake"
+          className="absolute right-1/3 top-40 transform -translate-y-1/2 w-auto h-auto"
+        />
+        <div className="flex justify-between md:flex-row flex-col md:py-7 relative">
+          <div className="flex flex-col items-center gap-10 md:gap-20 justify-center">
+            <div className="flex flex-col">
+              <h1 className="md:text-6xl text-4xl  font-bold bg-gradient-to-r  from-[#C91937] to-[#FDB646] text-transparent bg-clip-text  w-full text-center md:h-[122px] h-auto ">
+                Far from home?
+              </h1>
+              <p className="text-2xl px-5  text-center md:-mt-9">
+                we got you, checkout recipes from home chef's
+              </p>
             </div>
 
-            <hr className="flex-grow hidden md:block border-2 border-yellow-500" />
+            <div className="flex md:flex-row flex-col justify-between gap-10 md:gap-16">
+              <Link to="/signup">
+                <div className="flex items-center cursor-pointer justify-center rounded-tl-3xl shadow-custom rounded-br-3xl  bg-[#C91937] text-white w-[149px] h-[47px]">
+                  Register
+                </div>
+              </Link>
+              <Link to={"/recipes"}>
+                <div className="flex items-center cursor-pointer justify-center rounded-tl-3xl shadow-custom rounded-br-3xl  bg-[#FDB646] text-white w-[149px] h-[47px]">
+                  Explore
+                </div>
+              </Link>
+            </div>
+
+            <div className="flex  my-5 md:mt-20 items-center justify-center w-[100%] gap-10">
+              <img src={Twitter} alt="" />
+              <img src={Github} alt="" />
+
+              <div className="w-[45px] h-[45px] rounded-full p-[2px] bg-gradient-to-r from-red-500 to-yellow-500">
+                <div className="w-full h-full flex items-center justify-center rounded-full bg-white">
+                  <img src={din} alt="" className="" />
+                </div>
+              </div>
+
+              <hr className="flex-grow hidden md:block border-2 border-yellow-500" />
+            </div>
           </div>
-        </div>
-      
+
           <img
             src={Pizza}
             alt="pizza"
             className="md:w-[463px] w-full md:h-screen h-auto md:rounded-tl-[50px] md:rounded-br-[50px]"
           />
-      
+        </div>
       </div>
-    </div>
     </div>
   );
 };
