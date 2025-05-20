92445b6dfb32211d6f4c7067696c401edfc58852
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-05-20 21:35:43 +0100

    set up open api for receipes

---
+++ src/service/api.ts
@@ -0,0 +1,22 @@
+// src/api.ts
+import axios from "axios";
+
+const SPOONACULAR_API_KEY = "8e65ad3e90de48fcb358b80e0d02bfab";
+const BASE_URL = "https://api.spoonacular.com";
+
+const spoonacular = axios.create({
+  baseURL: BASE_URL,
+  params: {
+    apiKey: SPOONACULAR_API_KEY,
+  },
+});
+
+export const getRandomRecipes = async (number = 30) => {
+  const response = await spoonacular.get("/recipes/random", {
+    params: {
+      number,
+    },
+  });
+
+  return response.data.recipes;
+};
